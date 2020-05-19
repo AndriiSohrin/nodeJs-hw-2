f911627ca5412bc017befb2e3535441f24d0c0e4
AndriiSohrin <asohrin9996@gmail.com>
2020-05-19 17:34:38 +0300

    commit

---
+++ app.js
@@ -0,0 +1,53 @@
+const {rename, readdir, mkdir, rmdir} = require('fs')
+const path = require('path');
+
+
+mkdir(path.join(__dirname, 'test'), (err) => {
+    if (err) {
+        console.log(err)
+    }
+});
+
+const copy = (oldPath, newPath) => {
+    readdir(path.join(oldPath), (err, files) => {
+        if (err) {
+            console.log(err)
+        } else {
+            for (let i = 0; i < files.length; i++) {
+                rename(path.join(__dirname, oldPath, files[i]), path.join(__dirname, newPath, files[i]), (err) => {
+                    if (err) {
+                        console.log(err)
+                    }
+                })
+            }
+        }
+    })
+};
+
+
+const fun = () => {
+    return new Promise((resolve) => setTimeout(resolve));
+};
+fun()
+    .then(() => {
+        copy(`1800`, `test`);
+        return fun()
+    })
+
+    .then(() => {
+        copy(`2000`, `1800`);
+        return fun()
+    })
+
+    .then(() => {
+        copy(`test`, `2000`);
+        return fun()
+    })
+
+    .then(() => {
+        rmdir(path.join(__dirname, 'test'), (err) => {
+            if (err) {
+                console.log(err)
+            }
+        })
+    })
